defe4f2990c3d4d1b2661c4463550dc436c2e6e4
eulen avancena <avancena.eulz@gmail.com>
2025-10-29 21:35:12 +0800

    eulzdesktop10292025

--- SalesInventorySystem/TestSweepStakes.Designer.cs
+++ SalesInventorySystem/TestSweepStakes.Designer.cs
@@ -41,6 +41,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtavailable = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
@@ -48,10 +49,11 @@
             // txta
             // 
             this.txta.Font = new System.Drawing.Font("Tahoma", 32F, System.Drawing.FontStyle.Bold);
-            this.txta.Location = new System.Drawing.Point(12, 60);
+            this.txta.Location = new System.Drawing.Point(10, 49);
+            this.txta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txta.MaxLength = 1;
             this.txta.Name = "txta";
-            this.txta.Size = new System.Drawing.Size(70, 72);
+            this.txta.Size = new System.Drawing.Size(61, 59);
             this.txta.TabIndex = 0;
             this.txta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txta.TextChanged += new System.EventHandler(this.txta_TextChanged);
@@ -59,10 +61,11 @@
             // txtb
             // 
             this.txtb.Font = new System.Drawing.Font("Tahoma", 32F, System.Drawing.FontStyle.Bold);
-            this.txtb.Location = new System.Drawing.Point(88, 60);
+            this.txtb.Location = new System.Drawing.Point(75, 49);
+            this.txtb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtb.MaxLength = 1;
             this.txtb.Name = "txtb";
-            this.txtb.Size = new System.Drawing.Size(70, 72);
+            this.txtb.Size = new System.Drawing.Size(61, 59);
             this.txtb.TabIndex = 1;
             this.txtb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtb.TextChanged += new System.EventHandler(this.txtb_TextChanged);
@@ -70,10 +73,11 @@
             // txtc
             // 
             this.txtc.Font = new System.Drawing.Font("Tahoma", 32F, System.Drawing.FontStyle.Bold);
-            this.txtc.Location = new System.Drawing.Point(164, 60);
+            this.txtc.Location = new System.Drawing.Point(141, 49);
+            this.txtc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtc.MaxLength = 1;
             this.txtc.Name = "txtc";
-            this.txtc.Size = new System.Drawing.Size(70, 72);
+            this.txtc.Size = new System.Drawing.Size(61, 59);
             this.txtc.TabIndex = 2;
             this.txtc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtc.TextChanged += new System.EventHandler(this.txtc_TextChanged);
@@ -81,10 +85,11 @@
             // txtd
             // 
             this.txtd.Font = new System.Drawing.Font("Tahoma", 32F, System.Drawing.FontStyle.Bold);
-            this.txtd.Location = new System.Drawing.Point(240, 60);
+            this.txtd.Location = new System.Drawing.Point(206, 49);
+            this.txtd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtd.MaxLength = 1;
             this.txtd.Name = "txtd";
-            this.txtd.Size = new System.Drawing.Size(70, 72);
+            this.txtd.Size = new System.Drawing.Size(61, 59);
             this.txtd.TabIndex = 3;
             this.txtd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtd.TextChanged += new System.EventHandler(this.txtd_TextChanged);
@@ -92,10 +97,11 @@
             // txte
             // 
             this.txte.Font = new System.Drawing.Font("Tahoma", 32F, System.Drawing.FontStyle.Bold);
-            this.txte.Location = new System.Drawing.Point(316, 60);
+            this.txte.Location = new System.Drawing.Point(271, 49);
+            this.txte.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txte.MaxLength = 1;
             this.txte.Name = "txte";
-            this.txte.Size = new System.Drawing.Size(70, 72);
+            this.txte.Size = new System.Drawing.Size(61, 59);
             this.txte.TabIndex = 4;
             this.txte.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txte.TextChanged += new System.EventHandler(this.txte_TextChanged);
@@ -103,10 +109,11 @@
             // txtf
             // 
             this.txtf.Font = new System.Drawing.Font("Tahoma", 32F, System.Drawing.FontStyle.Bold);
-            this.txtf.Location = new System.Drawing.Point(392, 60);
+            this.txtf.Location = new System.Drawing.Point(336, 49);
+            this.txtf.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtf.MaxLength = 1;
             this.txtf.Name = "txtf";
-            this.txtf.Size = new System.Drawing.Size(70, 72);
+            this.txtf.Size = new System.Drawing.Size(61, 59);
             this.txtf.TabIndex = 5;
             this.txtf.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtf.TextChanged += new System.EventHandler(this.txtf_TextChanged);
@@ -115,17 +122,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 22.8F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Location = new System.Drawing.Point(28, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(409, 46);
+            this.label1.Size = new System.Drawing.Size(333, 37);
             this.label1.TabIndex = 6;
             this.label1.Text = "Digital Sweepstakes";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 200);
+            this.button1.Location = new System.Drawing.Point(10, 162);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(450, 39);
+            this.button1.Size = new System.Drawing.Size(386, 32);
             this.button1.TabIndex = 7;
             this.button1.Text = "ADD TICKET";
             this.button1.UseVisualStyleBackColor = true;
@@ -133,12 +141,10 @@
             // 
             // gridControl1
             // 
-            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.gridControl1.Location = new System.Drawing.Point(12, 246);
+            this.gridControl1.Location = new System.Drawing.Point(10, 200);
             this.gridControl1.MainView = this.gridView1;
-            this.gridControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(450, 275);
+            this.gridControl1.Size = new System.Drawing.Size(386, 223);
             this.gridControl1.TabIndex = 8;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -149,6 +155,7 @@
             this.gridView1.Appearance.HeaderPanel.Options.UseFont = true;
             this.gridView1.Appearance.Row.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gridView1.Appearance.Row.Options.UseFont = true;
+            this.gridView1.DetailHeight = 284;
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
             this.gridView1.OptionsBehavior.Editable = false;
@@ -159,9 +166,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 528);
+            this.button2.Location = new System.Drawing.Point(10, 429);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(450, 39);
+            this.button2.Size = new System.Drawing.Size(386, 32);
             this.button2.TabIndex = 9;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -170,9 +178,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 22.8F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(12, 143);
+            this.label2.Location = new System.Drawing.Point(10, 116);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(208, 46);
+            this.label2.Size = new System.Drawing.Size(170, 37);
             this.label2.TabIndex = 10;
             this.label2.Text = "Available:";
             // 
@@ -180,17 +188,28 @@
             // 
             this.txtavailable.AutoSize = true;
             this.txtavailable.Font = new System.Drawing.Font("Tahoma", 22.8F, System.Drawing.FontStyle.Bold);
-            this.txtavailable.Location = new System.Drawing.Point(213, 143);
+            this.txtavailable.Location = new System.Drawing.Point(183, 116);
             this.txtavailable.Name = "txtavailable";
-            this.txtavailable.Size = new System.Drawing.Size(44, 46);
+            this.txtavailable.Size = new System.Drawing.Size(37, 37);
             this.txtavailable.TabIndex = 11;
             this.txtavailable.Text = "0";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 22.8F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(460, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 37);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Available:";
+            // 
             // TestSweepStakes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 577);
+            this.ClientSize = new System.Drawing.Size(955, 469);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtavailable);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
@@ -203,6 +222,7 @@
             this.Controls.Add(this.txtc);
             this.Controls.Add(this.txtb);
             this.Controls.Add(this.txta);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "TestSweepStakes";
             this.Text = "TestSweepStakes";
             this.Load += new System.EventHandler(this.TestSweepStakes_Load);
@@ -228,5 +248,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label txtavailable;
+        private System.Windows.Forms.Label label3;
     }
 }
